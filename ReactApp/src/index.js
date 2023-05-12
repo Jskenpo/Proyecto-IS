efda9066c941190ac232e89aa90afdad170b2eb3
jsantis <san21153@uvg.edu.gt>
2023-05-12 16:41:03 -0600

    implementación de react

---
+++ ReactApp/src/index.js
@@ -0,0 +1,9 @@
+import {React, ReactDOM} from 'react';
+import App from './components/App';
+
+import './index.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
